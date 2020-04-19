cde7a32419561efcbb38871d875aa29279a75562
chrustkiran <christkiran.15@cse.mrt.ac.lk>
2020-04-20 00:54:18 +0530

    wrote controller test cases

--- src/test/java/com/example/Country/ControllerTest.java
+++ src/test/java/com/example/Country/ControllerTest.java
@@ -1,33 +1,22 @@
 package com.example.Country;
 
 import com.example.Country.controller.CountryController;
+import com.example.Country.model.CountryInfo;
 import com.example.Country.repositories.CountryInfoRepository;
 import com.example.Country.service.*;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
-import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.http.MediaType;
-import org.springframework.http.client.MultipartBodyBuilder;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.Assert;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.reactive.function.BodyInserters;
-import org.springframework.web.reactive.function.client.WebClient;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
-import reactor.core.publisher.Flux;
+import static org.assertj.core.api.Assertions.assertThat;
 
 
 import java.net.MalformedURLException;
@@ -46,7 +35,7 @@ public class ControllerTest {
     @Test
     void processTest() throws MalformedURLException {
 
-        final UrlResource resource = new UrlResource("file:/home/chrustkiran/Downloads/Country/example1.zip");
+        final UrlResource resource = new UrlResource("file:example.zip");
         MultiValueMap<String, UrlResource> data = new LinkedMultiValueMap<>();
         data.add("file", resource);
 
@@ -55,6 +44,14 @@ public class ControllerTest {
                 .uri("/process")
                 .contentType(MediaType.MULTIPART_FORM_DATA)
                 .body(BodyInserters.fromMultipartData(data))
+                .exchange().expectStatus().isOk()
+                .expectBodyList(CountryInfo.class).hasSize(9);
+    }
+
+    @Test
+    void findAllTest() {
+        webClient.get()
+                .uri("/findAll")
                 .exchange().expectStatus().isOk();
     }
 }
